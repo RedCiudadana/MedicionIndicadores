22a6ec7b92946f98f7722b1cee0fdeb653b57fed
Diego Auyón <diegoeauyonh@gmail.com>
2018-06-03 17:01:49 -0600

    Enable fingerprint exclude

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,11 +8,10 @@ module.exports = function(defaults) {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
       'importBootstrapCSS': false
-    }
-  //   , 
-  //   'fingerprint': {
-  //   exclude: ['img/*.png'],
-  // }
+    }, 
+    'fingerprint': {
+    exclude: ['img/*.png'],
+  }
   });
 
   app.import('bower_components/tabletop/src/tabletop.js');
