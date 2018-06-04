e9e53c50dd1a6636c44501ac789ddeab2442c9c4
Diego Auyón <diegoeauyonh@gmail.com>
2018-06-04 17:28:50 -0600

    Funcionando pagina compromises/id

--- app/routes/compromises/compromiso.js
+++ app/routes/compromises/compromiso.js
@@ -5,10 +5,20 @@ import ArrayProxy from '@ember/array/proxy';
 export default Route.extend({
   model(params) {
     var compromiso = ArrayProxy.create({ content: A(this.modelFor('compromises')) }).findBy('id', parseInt(params.compromiso_id));
-    var hitosDeCompromiso = this.modelFor('application').hitos.filterBy('compromiso', compromiso.compromiso);
+    var hitosDeCompromiso = this.modelFor('application').hitos.filterBy('compromiso', compromiso.compromiso).reverse();
+    var noMetas = hitosDeCompromiso.length;
+    var metasAtrasadas = hitosDeCompromiso.filterBy('estado','tarde').length;
+    var metasProceso = hitosDeCompromiso.filterBy('estado','incompleto').length;
+    var metasCompletadas = hitosDeCompromiso.filterBy('estado','completo').length;
     return {
       compromiso: compromiso,
-      hitosDeCompromiso: hitosDeCompromiso
+      hitosDeCompromiso: hitosDeCompromiso,
+      informacionCompromiso: {
+        noMetas: noMetas,
+        metasAtrasadas: metasAtrasadas,
+        metasProceso: metasProceso,
+        metasCompletadas: metasCompletadas
+      }
     };
   }
 });
